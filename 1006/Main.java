4b29d8fe9128acf9a336f7da76510b6e7bb90503
Kristyan Henrique <41088986+KristyanHenrique@users.noreply.github.com>
2022-09-14 11:29:36 -0300

    padronização de arquivos

---
+++ 1006/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner; 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        for (int i; i<3 ; i++) {
+            Sistem.out.printf(i+"\n");
+        }
+    }
+ 
+}
