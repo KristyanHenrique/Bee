f04bc59a101e3e59a7b9b4da8996d3590ea9ce15
Kristyan Henrique <41088986+KristyanHenrique@users.noreply.github.com>
2022-09-15 15:12:35 -0300

    Update Main.java

--- 2374/Main.java
+++ 2374/Main.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 import java.io.IOException;
 public class Main{
 	public static void main(String[] args) {
-	/* presentarion error 15% */
+		/* presentarion error 15% */
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int n2 = sc.nextInt();
 		n = n-n2;
-		System.out.println(n+"\n");
+		System.out.println(n);
 	}
 }
